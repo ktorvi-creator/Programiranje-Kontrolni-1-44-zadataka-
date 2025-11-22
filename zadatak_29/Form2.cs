7de17e6e4d11c44aff6fdf444858ebc7dc1acd0b
ktorvi-creator <ktorvibusiness@gmail.com>
2025-11-22 18:02:09 +0100

    Add files via upload

---
+++ zadatak_29/Form2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zadatak_29
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(string s)
+        {
+            InitializeComponent();
+            label1.Text = s;
+        }
+    }
+}
